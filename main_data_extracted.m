e2dd036f8d091265abb6040e76398afdfeb299a2
nairaditya <47370583+nairaditya@users.noreply.github.com>
2019-03-02 13:14:11 -0800

    Add files via upload

---
+++ main_data_extracted.m
@@ -0,0 +1,11 @@
+clear;clc;close all;
+
+%% figure 2
+
+open('figure2new_a.fig');
+a = get(gca,'Children');
+xdata = get(a, 'XData');
+ydata = get(a, 'YData');
+zdata = get(a, 'ZData');
+
+%% figure 2
